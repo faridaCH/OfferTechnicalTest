367ff9bb9dc29d50e8598a797108ada5ac46a611
faridaCH <farida.chelhi06@gmail.com>
2022-11-02 11:37:43 +0100

    create a UserRepository

---
+++ UserAPI/src/main/java/com/offertest/UserAPI/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.offertest.UserAPI.repositories;
+
+import com.offertest.UserAPI.entities.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<UserEntity,Integer> {
+
+}
